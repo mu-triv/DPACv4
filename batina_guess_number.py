66967e7a0b3d3d1969cb4a3ae2d5a06a0a64e2ed
VU Khac Tri <khactri.vu@sony.com>
2020-06-07 17:16:48 +0200

    add noise

--- batina_guess_number.py
+++ batina_guess_number.py
@@ -9,13 +9,21 @@ import pandas as pd
 max_mantissa_nbits = 23
 
 
-def build_guess_values(value_type='mantissa', numbers=None, mantissa_nbits=10, guess_range=None):
-    if value_type == 'mantissa':
+def build_guess_values(component='mantissa', numbers=None, mantissa_nbits=10, guess_range=None):
+    """
+    build the list of guess values which is used to evaluate the weight value
+    :param component: IEEE 754 component name, it must be "mentissa", "exponent", "sign"
+    :param numbers: the list of numbers of the previous state
+    :param mantissa_nbits: number of mantissa bits to be recovered
+    :param guess_range: the guess range
+    :return: the list of guess numbers
+    """
+    if component == 'mantissa':
         # set the exponent 1
         e = (0x7f << max_mantissa_nbits)
         guess_numbers = np.vectorize(int_to_float)(
             np.left_shift(np.arange(0, 1 << mantissa_nbits), max_mantissa_nbits - mantissa_nbits) | e)
-    elif value_type == 'exponent':
+    elif component == 'exponent':
         # remove the exponent bits in mantissa
         m = np.vectorize(lambda x: x & ~(0xff << max_mantissa_nbits))(np.vectorize(float_to_int)(numbers))
         # set all possible exponent bits
@@ -29,47 +37,92 @@ def build_guess_values(value_type='mantissa', numbers=None, mantissa_nbits=10, g
             hi_range = max(np.abs(guess_range))
             lo_range = max(np.min(guess_range), 0.0)
             guess_numbers = y[(lo_range <= y) & (y <= hi_range)]
-    elif value_type == 'sign':
+    elif component == 'sign':
         y = np.concatenate((np.asarray(numbers), -np.asarray(numbers)))
         if None is guess_range:
             guess_numbers = y
         else:
             guess_numbers = y[(guess_range[0] <= y) & (y <= guess_range[1])]
     else:
-        raise ValueError('the value_type is not supported')
+        raise ValueError('the component is not supported')
     return guess_numbers
 
 
-def compute_corr_numbers(weight_hw, known_inputs, guess_numbers, processing_func=None):
-    if None is processing_func:
-        processing_func = lambda x: float_to_bin(x).count('1')
+def build_input_values(component='mantissa',  mantissa_nbits=10):
+    """
+    build the list of input values which is used to evaluate the weight value
+    :param component: IEEE 754 component name, it must be "mentissa", "exponent", "sign"
+    :param mantissa_nbits: number of mantissa bits to be recovered
+    :return: the list of input numbers
+    """
+    total_mantissa_nbits = 23
+    if component == 'mantissa':
+        retval = np.vectorize(int_to_float)(
+            np.left_shift(np.arange(0, 2 << mantissa_nbits), total_mantissa_nbits - mantissa_nbits))
+    elif component == 'exponent':
+        retval = np.vectorize(int_to_float)(np.left_shift(np.arange(0, 2 << 8), total_mantissa_nbits))
+    elif component == 'sign':
+        retval = np.random.uniform(-1.0, 1.0, 10)
+    else:
+        raise ValueError('the component is not supported')
+    return retval
+
+
+def compute_corr_numbers(weight_hw, known_inputs, guess_numbers):
+    """
+    compute the HW correlations of the weight_hw and the HW of the results of the multiplication
+    between known_inputs and guess_numbers.
+    :param weight_hw: hamming weight of the known_inputs with the secret value
+    :param known_inputs: known input values
+    :param guess_numbers: guess numbers
+    :return:
+    """
     hw = pd.DataFrame(columns=guess_numbers,
-                      data=np.vectorize(processing_func)(np.asarray(known_inputs).reshape(-1, 1) * guess_numbers))
+                      data=np.vectorize(hamming_weight)(known_inputs.reshape(-1, 1) * guess_numbers))
     return hw.corrwith(pd.Series(weight_hw), method='pearson')
 
 
-def batina_recover_weight(secret_number, guess_range, mantissa_nbits=10, max_number_of_best_candidates=10):
+def batina_recover_weight(secret_number, guess_range, mantissa_nbits=10, max_number_of_best_candidates=10, noise=None):
+    """
+    recover the weight value (secret_number)
+    :param secret_number:
+    :param guess_range:
+    :param mantissa_nbits:
+    :param max_number_of_best_candidates:
+    :param noise: a tuple of (add_noise_function, signal_to_noise, frequency). The prototype of add_noise_function is,  func(signal, signal_to_noise, frequency)
+    :return: the 10 values which have highest HW correlations
+    """
+    if noise is not None:
+        add_noise_function, signal_to_noise, frequency = noise
+
     # step 1: guess the mantissa 10 bits
     total_mantissa_nbits = 23
-    known_inputs = np.vectorize(int_to_float)(
-        np.left_shift(np.arange(0, 2 << mantissa_nbits), total_mantissa_nbits - mantissa_nbits))
+    known_inputs = build_input_values(mantissa_nbits=mantissa_nbits, component='mantissa')
     weight_hw = np.vectorize(hamming_weight)(known_inputs * secret_number)
-    guess_numbers = build_guess_values(value_type='mantissa', mantissa_nbits=mantissa_nbits, guess_range=guess_range)
-    mantissa_processing_func = lambda x: bin(float_to_int(x) & 0x7fffff).count('1')
-    mantisa_corr = compute_corr_numbers(weight_hw, known_inputs, guess_numbers,
-                                        processing_func=mantissa_processing_func)
+    if noise is not None:
+        weight_hw = add_noise_function(weight_hw, signal_to_noise, frequency)
+    guess_numbers = build_guess_values(component='mantissa', mantissa_nbits=mantissa_nbits, guess_range=guess_range)
+    mantisa_corr = compute_corr_numbers(weight_hw, known_inputs, guess_numbers)
+
     # step 2: guess the exponent 8 bits
-    known_inputs = np.vectorize(int_to_float)(np.left_shift(np.arange(0, 2 << 8), total_mantissa_nbits))
+    known_inputs = build_input_values(component='exponent')
     weight_hw = np.vectorize(hamming_weight)(known_inputs * secret_number)
-    guess_numbers = build_guess_values(value_type='exponent', numbers=mantisa_corr.sort_values(ascending=False).index[
-                                                                      :max_number_of_best_candidates],
+    if noise is not None:
+        weight_hw = add_noise_function(weight_hw, signal_to_noise, frequency)
+    guess_numbers = build_guess_values(component='exponent',
+                                       numbers=mantisa_corr
+                                       .sort_values(ascending=False)
+                                       .index[:max_number_of_best_candidates],
                                        guess_range=guess_range)
     mantisa_exp_corr = compute_corr_numbers(weight_hw, known_inputs, guess_numbers)
+
     # step 3: guess the sign 1 bit
-    guess_numbers = build_guess_values(value_type='sign', numbers=mantisa_exp_corr.sort_values(ascending=False).index[
+    guess_numbers = build_guess_values(component='sign', numbers=mantisa_exp_corr.sort_values(ascending=False).index[
                                                                   :max_number_of_best_candidates],
                                        guess_range=guess_range)
-    known_inputs = np.random.uniform(-1.0, 1.0, 100)
+    known_inputs = build_input_values(component='sign')
     weight_hw = np.vectorize(hamming_weight)(known_inputs * secret_number)
+    if noise is not None:
+        weight_hw = add_noise_function(weight_hw, signal_to_noise, frequency)
     full_number_corr = compute_corr_numbers(weight_hw, known_inputs, guess_numbers)
     return full_number_corr.sort_values(ascending=False).iloc[:10]
